b892f0e983619c1ee84dd04707d4281ab7b0e647
Thainara Onofre <thainaraonofre@outlook.com>
2023-10-25 19:46:49 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/web/JPA/Hibernate/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.web.JPA.Hibernate.repositories;
+
+import com.web.JPA.Hibernate.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+
+
+}
