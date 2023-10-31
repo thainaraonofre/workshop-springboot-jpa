ad39e56645a34e045f95ba2d3fb2ca5f9b10abe8
Thainara Onofre <thainaraonofre@outlook.com>
2023-10-31 19:22:14 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/web/JPA/Hibernate/entities/Category.java
+++ src/main/java/com/web/JPA/Hibernate/entities/Category.java
@@ -1,5 +1,6 @@
 package com.web.JPA.Hibernate.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,7 +19,10 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+
     private Set<Product> products = new HashSet<>();
 
     public Category(){
