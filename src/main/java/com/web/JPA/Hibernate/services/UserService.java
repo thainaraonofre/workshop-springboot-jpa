8d57d28a66f221490a2d7de2ba07e18401af9d62
Thainara Onofre <thainaraonofre@outlook.com>
2023-11-14 21:53:11 -0300

    Exception handling - findById

--- src/main/java/com/web/JPA/Hibernate/services/UserService.java
+++ src/main/java/com/web/JPA/Hibernate/services/UserService.java
@@ -2,6 +2,7 @@ package com.web.JPA.Hibernate.services;
 
 import com.web.JPA.Hibernate.entities.User;
 import com.web.JPA.Hibernate.repositories.UserRepository;
+import com.web.JPA.Hibernate.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById (Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User obj) {
