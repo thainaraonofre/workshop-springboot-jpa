4d50d024f3e7b90d74311d41561618790006aaec
Thainara Onofre <thainaraonofre@outlook.com>
2023-11-16 23:01:46 -0300

    Exception handling - update

--- src/main/java/com/web/JPA/Hibernate/services/UserService.java
+++ src/main/java/com/web/JPA/Hibernate/services/UserService.java
@@ -4,6 +4,7 @@ import com.web.JPA.Hibernate.entities.User;
 import com.web.JPA.Hibernate.repositories.UserRepository;
 import com.web.JPA.Hibernate.services.exceptions.DatabaseException;
 import com.web.JPA.Hibernate.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
